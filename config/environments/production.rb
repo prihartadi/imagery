696db6b07bb34df978dba90e487e0980352954f8
Rizal Prihartadi <prihartadi@gmail.com>
2017-12-09 23:24:31 +0700

    install device auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Device install
+  config.action_mailer.default_url_options = { host: 'imagery95.herokuapp.com'}
+
 end
