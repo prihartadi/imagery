22f04031e9bff4331dcf1280b598e7960647489e
Rizal Prihartadi <prihartadi@gmail.com>
2017-12-12 00:42:28 +0700

    add pin image and username

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210132644) do
+ActiveRecord::Schema.define(version: 20171211171231) do
 
   create_table "pins", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
@@ -33,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171210132644) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
